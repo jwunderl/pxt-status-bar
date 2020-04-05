753ffac016aff00881be08f10b8cc9cecb861f78
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-04-04 21:43:28 -0700

    fix test file (remove main.ts that breaks it due to hoisting)

--- test.ts
+++ test.ts
@@ -1,12 +1,9 @@
-
-// TODO: file issue that test.ts isn't moved to be compiled last w/ same semantics as main.ts. Then uncomment and use this
-
-// const status = ui.statusbar.createSprite(50, 3, 0x7, 0x5, 50)
+const status = ui.statusbar.createSprite(50, 3, 0x7, 0x5, 50)
 // ui.statusbar.setFlag(status, StatusBarFlag.SmoothTransition, false);
-// // ui.statusbar.setFlag(status, StatusBarFlag.LabelAtEnd, true);
+// ui.statusbar.setFlag(status, StatusBarFlag.LabelAtEnd, true);
 
-// ui.statusbar.setCurrent(status, 40);
+ui.statusbar.setCurrent(status, 40);
 
-// ui.statusbar.setLabel(status, "HP", 0x7);
+ui.statusbar.setLabel(status, "HP", 0x7);
 
-// ui.statusbar.setBarBorder(status, 1, 0xb);
+ui.statusbar.setBarBorder(status, 1, 0xb);
